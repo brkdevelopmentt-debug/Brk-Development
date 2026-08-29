b67e4213811eb62c717d983e09bae2af43e892cd
brkdevelopmentt-debug <brkdevelopmentt@gmail.com>
2026-08-30 00:02:10 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ana sayfa doğrudan login.html açılacak
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Dashboard rotası
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
 app.listen(port, () => {
-    console.log(`[Brk Devs] fake-players paneli aktif: ${port}`);
+    console.log(`[Brk Development] Sunucu aktif: ${port}`);
 });
